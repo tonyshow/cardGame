d94d7263b9b05fe31a203082e0885d66885b37ef
tony <994262676@qq.com>
2016-01-14 10:23:36 +0800

    新增

---
+++ Assets/Resources/Script/Fight/Controller/FightController.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class FightController
+{
+    public enum RIGHTTOPLAY
+    {
+        NONE,
+        ENEMY,  //敌方出牌权
+        ENEMYING,  //敌方出牌权
+        MINEING,    //我方出牌权
+        MINE    //我方出牌权
+    }
+    //单列模式
+    public static FightController instance = null;
+    public static FightController getInstance()
+    {
+        if (instance == null)
+        {
+            instance = new FightController();
+        }
+        return instance;
+    }
+    private RIGHTTOPLAY rightToPlay = RIGHTTOPLAY.NONE;
+    public RIGHTTOPLAY RightToPlay
+    {
+        set { rightToPlay = value; }
+        get { return rightToPlay; }
+    }
+      
+}
