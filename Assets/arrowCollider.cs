ca91fa256d720666625eef114f4695059dd7aff3
tonyshow <994262676@qq.com>
2016-01-14 23:08:20 +0800

    战斗修改

---
+++ Assets/arrowCollider.cs
@@ -0,0 +1,11 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class arrowCollider : MonoBehaviour {
+
+	// Use this for initialization
+    void OnCollisionEnter(Collision coll)
+    {
+        Debug.Log("箭头遇到碰撞");
+	}  
+}
