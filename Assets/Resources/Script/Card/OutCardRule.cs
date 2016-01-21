49cc05dd98b8577092ab5ac0856adb3c5df88005
tony <994262676@qq.com>
2016-01-21 18:29:21 +0800

    规则

--- Assets/Resources/Script/Card/OutCardRule.cs
+++ Assets/Resources/Script/Card/OutCardRule.cs
@@ -26,25 +26,235 @@ public enum OUTCARDRULE
     FIVE
 }
 
-
+public class sortCardRule
+{
+    public OUTTYPE type;
+    public Dictionary<int,int> data;
+}
 public class OutCardRule
 {
-    OutCardRule()
-    {      
-        List<OUTTYPE> list = new List<OUTTYPE>();
-        list.Add(OUTTYPE.Single);
-        list.Add(OUTTYPE.Sub);
-        list.Add(OUTTYPE.threeAndTwo);
-        list.Add(OUTTYPE.threeAndOne);
-        list.Add(OUTTYPE.twoSub);
-        list.Add(OUTTYPE.straight);
-        list.Add(OUTTYPE.bomb);
-        list.Add(OUTTYPE.kingBomb);  
-    }
-    static private Dictionary<OUTCARDRULE, List<OUTTYPE>> AllRule = new Dictionary<OUTCARDRULE, List<OUTTYPE>>();
-    static public List<OUTTYPE> getOutCardRule( OUTCARDRULE rule )
+
+    private static Dictionary<OUTCARDRULE, Dictionary<OUTTYPE, int>> AllRule = new Dictionary<OUTCARDRULE, Dictionary<OUTTYPE, int>>();
+    //单列模式
+    public static OutCardRule instance = null;
+    public static OutCardRule getInstance()
     {
-        return AllRule[rule];        
+        if (instance == null)
+        {
+            instance = new OutCardRule();
+            instance.init();
+        }
+        return instance;
     }
 
+    private void init()
+    {
+        Dictionary<OUTTYPE, int> dic = new Dictionary<OUTTYPE, int>();
+        dic.Add(OUTTYPE.Single, 1);
+        dic.Add(OUTTYPE.threeAndTwo, 3);
+        dic.Add(OUTTYPE.Sub, 2); 
+        dic.Add(OUTTYPE.threeAndOne, 4);
+        dic.Add(OUTTYPE.twoSub, 5);
+        dic.Add(OUTTYPE.straight, 6);
+        dic.Add(OUTTYPE.bomb, 7);
+        dic.Add(OUTTYPE.kingBomb, 8);
+        AllRule.Add(OUTCARDRULE.ONE, dic);  
+    }
+
+
+
+    private Dictionary<int, int> subNumber(Dictionary<int, int> data)
+    {
+        Dictionary<int, int> temp = new Dictionary<int, int>(); 
+        foreach (var item in data)
+        {
+            int key = item.Key;
+            int v = item.Value;
+            foreach (var item1 in data)
+            {
+                int key1 = item1.Key;
+                int v1 = item1.Value;
+                if (key != key1 && v == v1)
+                {
+                    temp.Add(key, v);
+                }
+            }
+        }
+        return temp;
+    }
+
+     
+
+    private Dictionary<int, int> isHaveThree(Dictionary<int, int> data)
+    {
+        Dictionary<int, int> sub = subNumber(data);
+        Dictionary<int, int> temp = new Dictionary<int, int>();
+        
+        if ( sub.Count == 1)
+        {
+            int haveNum=-1;
+            foreach (var item in data)
+            {
+                haveNum = item.Value;
+            }
+          
+            foreach (var item1 in data)
+            {
+                if (item1.Value == haveNum )
+                {
+                    temp.Add(item1.Key, item1.Value);
+                }
+            }
+        }
+        return temp;
+    }
+
+   
+    private Dictionary<int, int> isHaveStraight(Dictionary<int, int> data)
+    {
+        Dictionary<int, int> temp = new Dictionary<int, int>();
+        if (isHaveThree(data).Count == 0 )
+        {
+            foreach (var item in data)
+            {
+                int num = item.Value;
+
+                int key1_2 = -1;
+                int key1_3 = -1;
+                int pos1_2 = -1;
+                int pos1_3 = -1;
+
+                foreach (var item1 in data)
+                {
+                    //为顺子第一位
+                    int tempNum1_2= num +1;
+                    int tempNum1_3= num+2;
+
+                    if (item1.Value == tempNum1_2)
+                    {
+                        pos1_2 = item1.Value;
+                    }
+                    if (item1.Value == tempNum1_3)
+                    {
+                        pos1_3 = item1.Value;
+                    }
+
+                    //存在顺子
+                    if (key1_2 > 1 && pos1_2 != -1 && pos1_3 != -1)
+                    {
+                        temp.Add(item.Key, item.Value);
+                        temp.Add(key1_2, pos1_2);
+                        temp.Add(key1_3, pos1_2);
+                        break;
+                    }
+
+                    //为顺子第二位
+                    tempNum1_2 = num - 1;
+                    tempNum1_3 = num + 1;
+                    if (item1.Value == tempNum1_2)
+                    {
+                        pos1_2 = item1.Value;
+                    }
+                    if (item1.Value == tempNum1_3)
+                    {
+                        pos1_3 = item1.Value;
+                    }
+
+                    //存在顺子
+                    if (tempNum1_2 > 0 && pos1_2 != -1 && pos1_3 != -1)
+                    {
+                        temp.Add(item.Key, item.Value);
+                        temp.Add(key1_2, pos1_2);
+                        temp.Add(key1_3, pos1_2);
+                        break;
+                    }
+
+                    //为顺子第二位
+                    tempNum1_2 = num - 1;
+                    tempNum1_3 = num - 2;
+                    if (item1.Value == tempNum1_2)
+                    {
+                        pos1_2 = item1.Value;
+                    }
+                    if (item1.Value == tempNum1_3)
+                    {
+                        pos1_3 = item1.Value;
+                    }
+
+                    //存在顺子
+                    if (tempNum1_2 > 1 && pos1_2 != -1 && pos1_3 != -1)
+                    {
+                        temp.Add(item.Key, item.Value);
+                        temp.Add(key1_2, pos1_2);
+                        temp.Add(key1_3, pos1_2);
+                        break;
+                    }
+
+                }
+            }
+        }
+        return temp;
+    }
+
+    private Dictionary<int, int> isHaveBomb(Dictionary<int, int> data)
+    {
+        Dictionary<int, int> temp = new Dictionary<int, int>();
+        return temp;
+    }
+    
+    
+    //获得出牌规则
+    //返回的是出牌的规则
+    public void getTip( OUTCARDRULE outCardRule , Dictionary<int,int> data )
+    {
+        //出牌顺序
+        Dictionary<OUTTYPE, int> outRule = AllRule[outCardRule];
+
+        List<OUTTYPE> outType = new List<OUTTYPE>();
+        //单必有
+        foreach (var key in outRule.Keys)
+        { 
+            OUTTYPE type =  (OUTTYPE)key;
+            //必须有单
+            if (  OUTTYPE.Single == type )
+            {
+                outType.Add(type);
+            }
+            else if (OUTTYPE.Sub == type)
+            {
+                if (this.subNumber(data).Count > 0)
+                {
+                    outType.Add(type);
+                }
+            }
+            else if (OUTTYPE.threeAndTwo == type)
+            {
+                if ( data.Count ==5 && this.isHaveThree(data).Count > 0)
+                {
+                    outType.Add(type);
+                }
+            }
+            else if (OUTTYPE.threeAndOne == type)
+            {
+                if (data.Count >= 4 && this.isHaveThree(data).Count > 0)
+                {
+                    outType.Add(type);
+                }
+            }
+            else if (OUTTYPE.twoSub == type)
+            {
+                if ( subNumber(data).Count == 2 )
+                {
+                    outType.Add(type);
+                }
+            }
+            else if (OUTTYPE.straight == type)
+            {
+                if (isHaveStraight(data).Count >= 3)
+                {
+                    outType.Add(type);
+                }
+            }            
+        } 
+    }
 }
